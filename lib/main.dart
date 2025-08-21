6048b382c9619cc9612650bb015599037af89009
harshmaur2004 <harshmaur2004@gmail.com>
2025-08-21 19:45:14 +0000

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:myapp/screens/home_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
+void main() async {
+  //initialize widget bindings
+  WidgetsFlutterBinding.ensureInitialized();
+  //Initialize Firebase with the platform
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
-void main() {
   runApp(const MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Home_Page()
-    );
+    return const MaterialApp(home: Home_Page());
   }
 }
